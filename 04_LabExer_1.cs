ce55e15e714fd2432eb0335aa03b8ed5ced1c735
Balisi, J <jasperbalisi05@gmail.com>
2024-09-04 10:45:36 +0800

    Add files via upload

---
+++ 04_LabExer_1.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+class appleMarket
+{
+    static void Main (string[] args)
+    {
+        double apple = 32.50;
+
+        Console.Write("Enter the number of apples you want to purchase: ");
+        double buyApple = Convert.ToDouble(Console.ReadLine());
+        double valueApple = apple * buyApple;
+
+        Console.Clear(); 
+        Console.Write("The total price of " + buyApple + " is " + valueApple);
+        Console.WriteLine("\n------------------------------------------");
+        Console.Write("The value of the converted price is: " + Math.Floor(valueApple));
+    }
+}
+
+
+
